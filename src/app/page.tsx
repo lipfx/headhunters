508c0cee8d744cfa41ab618e7dce3c55b7184fd7
Filip Cerinski <68952723+lipfx@users.noreply.github.com>
2024-03-17 19:32:21 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+
+export default function Home() {
+
+ 
+
+  return (
+    <>
+        <div className="spacing1">
+        <button className="" >HOME</button>
+        <button className="">BUTTON 2</button>
+      <button className="">BUTTON 3</button>
+      <button className="">Button 4</button>
+      <button className="">Button 5</button>
+      <button className="">Button 6</button>
+    </div>
+        <div className= 'spacing2'>
+          <p>PAGE </p>
+        </div>
+        <div className= 'spacing3'>
+        <button>CLICK 2 SCROLL</button>
+        </div>
+        <div className= 'spacing4' id='page1'>
+          <p>PAGE 1</p>
+        </div>
+      </>
+  );
+}
