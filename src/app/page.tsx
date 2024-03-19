8f1201604e00fe012f7f5bb73c27ccea7398a4b1
Filip Cerinski <68952723+lipfx@users.noreply.github.com>
2024-03-19 19:31:58 +0100

    abit of changes
    home bar
    pic resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 import HH from './HH.png';
 import HH2 from './ezgif.com-video-to-gif_5.gif';
 
+
 export default function Home() {
+
+ 
+
   return (
     <>
       <div className="spacing1">
@@ -12,6 +16,9 @@ export default function Home() {
         <button className="">PAGE 3</button>
         <button className="">PAGE 4</button>
         <button className="">PAGE 5</button>
+
+    
+        
       </div>
       <div className='spacing2'>
         <Image src={HH} alt="Example GIF" />
@@ -25,6 +32,9 @@ export default function Home() {
         <p>PAGE 1</p>
         <p>ja sam pr0 </p>
         <Image className='position' src={HH2} alt="Your GIF" />
+
+    
+
       </div>
       <div className='spacing5'>
         <button>back 2 top #1 BRAH</button>
@@ -33,3 +43,4 @@ export default function Home() {
     </>
   );
 }
+
